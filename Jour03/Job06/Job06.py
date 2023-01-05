47b7bc1f1c4c737200976d044a0ab40b14379abd
raphael-rossignol <raphael.rossignol@laplateforme.io>
2023-01-05 10:03:30 +0100

    added job06, kinda working

--- Jour03/Job06/Job06.py
+++ Jour03/Job06/Job06.py
@@ -1,6 +1,15 @@
-def pyramide():
-    alphabet = "abcdefghijklmnopqrstuvwxyz" * 10
-    while
-    print(alphabet[:1])
-
-pyramide()
+string = "abcdefghijklmnopqrstuvwxyz" * 10
+n = len(string)
+level = 0
+while n > 0:
+  count = 2 ** level
+  if n >= count:
+    print(string[:count])
+  else:
+    if level % 2 == 0:
+      print(string)
+    else:
+      print(string[::-1])
+  string = string[count:]
+  n = len(string)
+  level += 1
